9198412cf81b1dd0f156a5538433d97b5785fd5c
Tuan Dau <tukauu@gmail.com>
2022-04-14 02:07:55 -0700

    feat: added header to website

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  darkmode: false,
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': "#565666",
+        'input': '#2A2A35'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
